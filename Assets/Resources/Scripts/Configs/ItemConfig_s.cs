28e19dc7436aac46148defdc164619c15192b04d
narlon <narlonzjh@163.com>
2025-08-18 22:04:22 +0800

    add item

---
+++ Assets/Resources/Scripts/Configs/ItemConfig_s.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CommonConfig
+{
+    public class ItemConfig
+    {
+        /// <summary>
+        ///序列
+        /// </summary>
+        public int Id;
+        /// <summary>
+        ///名字
+        /// </summary>
+        public string Name;
+        /// <summary>
+        ///等级
+        /// </summary>
+        public int Lv;
+        /// <summary>
+        ///统帅
+        /// </summary>
+        public int LeadShip;
+        /// <summary>
+        ///智力
+        /// </summary>
+        public int Inte;
+        /// <summary>
+        ///武力
+        /// </summary>
+        public int Str;
+        /// <summary>
+        ///生命
+        /// </summary>
+        public int Hp;
+        /// <summary>
+        ///移动速度
+        /// </summary>
+        public int MoveSpeed;
+        /// <summary>
+        ///攻击距离
+        /// </summary>
+        public int Range;
+        /// <summary>
+        ///技能
+        /// </summary>
+        public int[] Skills;
+        /// <summary>
+        ///hit
+        /// </summary>
+        public string HitEffect;
+        /// <summary>
+        ///背景图
+        /// </summary>
+        public string Icon;
+
+
+        public ItemConfig(int Id, string Name, int Lv, int LeadShip, int Inte, int Str, int Hp, int MoveSpeed, int Range, int[] Skills, string HitEffect, string Icon)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Lv = Lv;
+            this.LeadShip = LeadShip;
+            this.Inte = Inte;
+            this.Str = Str;
+            this.Hp = Hp;
+            this.MoveSpeed = MoveSpeed;
+            this.Range = Range;
+            this.Skills = Skills;
+            this.HitEffect = HitEffect;
+            this.Icon = Icon;
+
+        }
+
+        public ItemConfig() { }
+
+        private static Dictionary<int, ItemConfig> config = new Dictionary<int, ItemConfig>();
+        public static Dictionary<int, ItemConfig>.ValueCollection ConfigList
+        {
+            get
+            {
+                return config.Values;
+            }
+        }
+
+        public static void Refresh(Dictionary<int, ItemConfig> dict)
+        {
+            config.Clear();
+            config = dict;
+        }
+
+        public static void Load()
+        {
+            config.Clear();
+            config[400001] = new ItemConfig(400001, "关王刀", 1, 0, 0, 0, 0, 0, 0, null, "SwordHitYellowCritical", "guanwangdao");
+
+        }
+
+        public static ItemConfig GetConfig(int id)
+        {
+            ItemConfig data;
+            if (config.TryGetValue(id, out data))
+            {
+                return data;
+            }
+            throw new NullReferenceException(string.Format("配置表ItemConfig不存在id={0}", id));
+        }
+
+        public static bool HasConfig(int id)
+        {
+            if (config.ContainsKey(id))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void Assign(int id, ItemConfig configData)
+        {
+            config[id] = configData; 
+        }
+
+        public static void Add(int id, ItemConfig configData)
+        {
+            if (!config.ContainsKey(id))
+            {
+                config.Add(id, configData);
+            }
+        }
+
+        public static void Remove(int id)
+        {
+            if (config.ContainsKey(id))
+            {
+                config.Remove(id);
+            }
+        }
+    }
+}
