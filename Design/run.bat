c3ea62982b6ea99eb800e54e241765332034b46f
narlon <narlonzjh@163.com>
2025-12-01 13:25:02 +0800

    add cfg

---
+++ Design/run.bat
@@ -0,0 +1,12 @@
+Excel2dll.exe -config config -s -out out
+
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\HeroConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\SkillConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\BuffConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\ItemConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\SoldierConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\ShopConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\PlayerConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\HeroFriendConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\FormulaLearnAttrConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
+copy "D:\U3dPrj\SanTeam\Design\out\Csharp_s\JobConfig_s.cs" "D:\U3dPrj\SanTeam\Assets\Resources\Scripts\Configs\"
